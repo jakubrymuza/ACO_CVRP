7cf225a00ffa4605b3172d49e026490b36acdd36
Jakub Rymuza <jakubrymuza@gmail.com>
2023-03-24 23:55:57 +0100

    add greedy algorithm

---
+++ CVRP_project/Algorithms/GreedyAntAlgorithm.cs
@@ -0,0 +1,13 @@
+﻿using CVRP_project.Structures;
+
+namespace CVRP_project.Algorithms
+{
+    internal class GreedyAntAlgorithm : BasicAntAlgorithm, IAlgorithm
+    {
+        // w algorytmie zachłannym wystarczy ustawić parametr alfa na 0, wówczas miasta będą wybierane zachłannie
+        public GreedyAntAlgorithm(ProblemInstance cities, int iterationsCount, int antsCount) : base(cities, iterationsCount, antsCount, 0, 1, 0, 0)
+        { }
+
+        public new String Name() => "Greedy Ant Algorithm";
+    }
+}
