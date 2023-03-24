9fc274545b08c314e08ccd75ddc968af963863e0
Jakub Rymuza <jakubrymuza@gmail.com>
2023-03-24 23:55:35 +0100

    add elitist ant algorithm

---
+++ CVRP_project/Algorithms/ElitistAntAlgorithm.cs
@@ -0,0 +1,20 @@
+﻿using CVRP_project.Structures;
+
+namespace CVRP_project.Algorithms
+{
+    internal class ElitistAntAlgorithm : BasicAntAlgorithm, IAlgorithm
+    {
+        private int ElitistAntsCount;
+        public ElitistAntAlgorithm(ProblemInstance cities, int iterationsCount, int antsCount, double alpha, double beta, double evaporation, double pheromoneStrength, int elitistAntsCount) : base(cities, iterationsCount, antsCount, alpha, beta, evaporation, pheromoneStrength)
+        {
+            ElitistAntsCount = elitistAntsCount;
+        }
+
+        public new String Name() => "Elitist Ant Algorithm";
+
+        protected new void PostAddNewPheromone()
+        {
+            // TODO
+        }
+    }
+}
