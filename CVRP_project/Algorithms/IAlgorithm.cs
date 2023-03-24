aeb293f44072fbda2c4399095e3a9deaf3b8cb3e
Jakub Rymuza <jakubrymuza@gmail.com>
2023-03-24 23:56:15 +0100

    add ranked ant algorithm

---
+++ CVRP_project/Algorithms/IAlgorithm.cs
@@ -0,0 +1,11 @@
+﻿namespace CVRP_project.Algorithms
+{
+    internal interface IAlgorithm
+    {
+        public (List<int> route, double routeLength) Solve(int seed);
+
+        public void Reset();
+
+        public String Name();
+    }
+}
---
+++ CVRP_project/Algorithms/RankedAntAlgorithm.cs
@@ -0,0 +1,21 @@
+﻿using CVRP_project.Structures;
+
+namespace CVRP_project.Algorithms
+{
+    internal class RankedAntAlgorithm : BasicAntAlgorithm, IAlgorithm
+    {
+        private int RankingSize;
+
+        public RankedAntAlgorithm(ProblemInstance cities, int iterationsCount, int antsCount, double alpha, double beta, double evaporation, double pheromoneStrength, int rankingSize) : base(cities, iterationsCount, antsCount, alpha, beta, evaporation, pheromoneStrength)
+        {
+            RankingSize = rankingSize;
+        }
+
+        public new String Name() => "Greedy Ant Algorithm";
+
+        protected new void AddNewPheromone()
+        {
+            // TODO
+        }
+    }
+}
